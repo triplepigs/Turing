8bb5696d1627c7577a4d3aa2680db21f51be5ab7
isenguard13 <145545578+isenguard13@users.noreply.github.com>
2024-02-22 21:24:44 +0800

    TuringSolve.cpp

--- TuringSolve.cpp
+++ TuringSolve.cpp
@@ -118,12 +118,54 @@ int TuringOddMore(vector<int>& code){
     }
 }
 
-int TuringPOdd(vector<int>& code){
+int TuringPParity(vector<int>& code){
     if(code[2] % 2 == 0){
-        return 0;
-    }
-    return 1;
+		return 0;
+	}else{
+		return 1;
+	}
 }
+
+int TuringBCmp1(vector<int>& code){
+    if(code[0] == 1){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+int TuringSmall(vector<int>& code){
+    if(code[0] < code[1] && code[0] < code[2]){
+        return 0;
+    } else if(code[1] < code[0] && code[1] < code[2]){
+        return 1;
+    } else if(code[2] < code[1] && code[2] < code[0]){
+        return 2;
+    } else{
+        return -1;
+    }
+}
+
+int TuringYCmpP(vector<int>& code){
+    if(code[1] < code[2]){
+        return 0;
+    }else if(code[1] == code[2]){
+        return 1;
+    }else{
+        return 2;
+    }
+}
+
+int Turing4s(vector<int>& code){
+    int num = 0;
+	    for(int i : code){
+	        if(i == 4){
+	            num++;
+	        }
+	    }
+	    return num;
+}
+
 
 void printCode(vector<int>& code){
     cout<<"("<<code[0]<<" "<<code[1]<<" "<<code[2]<<")"<<endl;
@@ -140,7 +182,11 @@ std::function<int(vector<int>&)> functions[] = {
     TuringOddMore,    //7 偶数多还是奇数多
     TuringPCmp3,    //8 紫色和3比大小
     TuringBCmpY,    //9 蓝色和黄色比大小
-    TuringPOdd,     //10 紫色是偶数吗
+    TuringPParity,  //10 紫色的奇偶性
+    TuringBCmp1, //11 蓝色比1大小
+    TuringSmall, //12 哪个颜色的数字严格最小
+    TuringYCmpP, //13 黄色和紫色比较
+    Turing4s, //14 有几个4
     // ... 添加其他函数
 };
 
@@ -171,6 +217,9 @@ bool test4Code(vector<int>& code, int a, int b, int c, int d){
                     && functions[c](code) == functions[c](code2) && functions[d](code) == functions[d](code2)){
                         return false;
                     }
+                    if(functions[a](code) == -1 || functions[b](code) == -1 || functions[c](code) == -1 || functions[d](code) == -1){
+						return false;
+					}
                 }
             }
         }
@@ -182,10 +231,10 @@ int main(){
     vector<vector<int>> ans;
     
     //需要验证的图灵机的编号
-    int a = 4;
-    int b = 5;
-    int c = 6;
-    int d = 10;
+    int a = 11;
+    int b = 12;
+    int c = 13;
+    int d = 14;
     for(int i = 1; i <= 5; i++){
         for(int j = 1; j <= 5; j++){
             for(int k = 1; k <= 5; k++){
